e033eac8d02051df3094fa5784c6261d0597d08c
Dominic <dom42486@gmail.com>
2016-06-07 23:12:02 -0700

    added styling and created CRUD for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   def index
     @posts = Post.all
@@ -8,8 +9,34 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
+  def edit
+  end
+
+  def show
+  end
+
   def create
-    @post = Post.create(post_params)
+     if @post = Post.create(post_params)
+       flash[:success] = "Your post has been created!"
+       redirect_to posts_path
+     else
+       flash.now[:alert] = "Your new post couldn't be created! Please check the form."
+       render :new
+     end
+  end
+
+  def update
+    if @post.update(post_params)
+      flash[:success] = "Post updated."
+      redirect_to posts_path
+    else
+      flash.now[:alert] = "Update failed. Please check the form."
+      render :edit
+    end
+  end
+
+  def destroy
+    @post.destroy
     redirect_to posts_path
   end
 
@@ -18,6 +45,10 @@ class PostsController < ApplicationController
 
   def post_params
     params.require(:post).permit(:image, :caption)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 
 end
